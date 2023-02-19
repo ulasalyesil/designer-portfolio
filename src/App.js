fbcb24cabe6daec5c537bb8a87849c91120c1016
Ulaş Alyeşil <umutulas08@gmail.com>
2023-02-19 15:19:07 +0300

    installed inspect package

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Downloads from './components/downloads';
 import Hero from './components/hero';
 import Products from './components/products';
 import Footer from './components/footer';
+import Inspect from 'inspx';
 
 function App() {
   return (
-
     <div className="App">
       <div id='main-wrapper' className='bg-zinc-900 w-screen h-full flex flex-col place-content-center'>
       <div id='content' className='flex flex-col p-16 mb-16 m-auto gap-16 max-w-2xl'>

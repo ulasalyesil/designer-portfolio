456884cbd77724b76865b088a525d39c8bc99840
Ulaş Alyeşil <ulasalyesil@gmail.com>
2023-02-21 18:30:37 +0300

    home page padding top fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Inspect>
     <div className="App">
       <div id='main-wrapper' className='bg-zinc-900 w-screen h-full flex flex-col place-content-center'>
-      <div id='content' className='flex flex-col p-4 mb-16 m-auto gap-16 max-w-2xl'>
+      <div id='content' className='flex flex-col p-4 pt-16 mb-16 m-auto gap-16 max-w-2xl'>
 
         <Hero />
         <Products />

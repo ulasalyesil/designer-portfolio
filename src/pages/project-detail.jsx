e1f07a6fcfca5eec1f84fbb3efa27f994f33bb40
Ulaş Alyeşil <umutulas08@gmail.com>
2023-02-22 07:05:50 +0300

    Updated for errors

--- src/pages/project-detail.jsx
+++ src/pages/project-detail.jsx
@@ -1,7 +1,6 @@
 import image from "../assets/project-icon-1.png"
 import Footer from "../components/footer";
 import Inspect from "inspx";
-import AxeMode from 'axe-mode';
 import { Link } from "react-router-dom";
 
 function ProjectDetailPage() {
@@ -12,7 +11,7 @@ function ProjectDetailPage() {
       <div id='content' className='flex flex-col p-4 pt-16 mb-16 m-auto gap-8 max-w-2xl sm:pt-32 pb-60'>
         <h1 className="text-2xl text-zinc-200 text-left">Project Name</h1>
         <div>
-            <img src={image} className="rounded-2xl w-80"/>
+            <img src={image} className="rounded-2xl w-80" alt="project"/>
         </div>
           <Link to="/">
             <button className="bg-zinc-100 p-2 w-full rounded-full shadow-xl shadow-zinc-100/16 hover:shadow-2xl hover:shadow-zinc-100-30">Go back</button>
